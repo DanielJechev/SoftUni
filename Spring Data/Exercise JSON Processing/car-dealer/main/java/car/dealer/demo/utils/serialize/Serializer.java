37085270a83997d57de83a5a6489d5c8a6307f64
DanielJechev <jechew@abv.bg>
2020-03-19 12:18:46 +0200

    new

--- Spring Data/Exercise JSON Processing/car-dealer/main/java/car/dealer/demo/utils/serialize/Serializer.java
+++
@@ -1,8 +0,0 @@
-package car.dealer.demo.utils.serialize;
-
-public interface Serializer {
-
-    <T> void serialize(T t, String fileName);
-
-    <T> T deserialize(Class<T> clazz, String fileName);
-}
--- Spring Data/Exercise JSON Processing/product-shop/main/java/json/processing/util/serialize/Serializer.java
+++
@@ -1,8 +0,0 @@
-package json.processing.util.serialize;
-
-public interface Serializer {
-
-    <T> void serialize(T t, String fileName);
-
-    <T> T deserialize(Class<T> clazz, String fileName);
-}
